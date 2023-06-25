077618cc57deef113c021964b49fd33b0d827cad
Stanislav Benet <stanislav.benet@gmail.com>
2023-06-25 22:42:14 +0300

    Add CRUD for fighter

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -7,7 +7,153 @@ import {
 } from "../middlewares/fighter.validation.middleware.js";
 
 const router = Router();
-
 // TODO: Implement route controllers for fighter
+
+router.get(
+  "/",
+  (req, res, next) => {
+    try {
+      const fighters = fighterService.getAllFighters();
+      if (fighters) {
+        req.body = {
+          fighters,
+        };
+        return req.body;
+      }
+    } catch ({ message }) {
+      return (req.body = {
+        error: true,
+        message,
+      });
+    } finally {
+      next();
+    }
+  },
+
+  responseMiddleware
+);
+
+router.get(
+  "/:id",
+  (req, res, next) => {
+    let id = req.params.id;
+    try {
+      const fighter = fighterService.searchFighter({
+        id,
+      });
+      if (fighter) {
+        req.body = {
+          ...fighter,
+        };
+        return req.body;
+      } else {
+        throw new Error(`Fighter not found`);
+      }
+    } catch ({ message }) {
+      return (req.body = {
+        error: true,
+        message,
+      });
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+router.post(
+  "/",
+  createFighterValid,
+  (req, res, next) => {
+    try {
+      const { error, message } = req.body;
+      if (error) {
+        throw new Error(message);
+      }
+      const { name } = req.body;
+      const currentFighter = fighterService.searchFighter({
+        name: name.toLowerCase(),
+      });
+      if (!currentFighter) {
+        const fighter = fighterService.addFighter({
+          ...req.body,
+          name: name.toLowerCase(),
+        });
+        req.body = {
+          ...fighter,
+        };
+        return req.body;
+      } else {
+        throw new Error("Fighter not found");
+      }
+    } catch ({ message }) {
+      return (req.body = {
+        error: true,
+        message,
+      });
+    } finally {
+      next();
+    }
+  },
+
+  responseMiddleware
+);
+
+router.put(
+  "/:id",
+  updateFighterValid,
+  (req, res, next) => {
+    let id = req.params.id;
+    const { error, message } = req.body;
+    try {
+      if (error) {
+        throw new Error(message);
+      }
+      const fighter = fighterService.updateFighter(id, req.body);
+      if (fighter) {
+        req.body = {
+          ...fighter,
+        };
+        return req.body;
+      } else {
+        throw new Error(`Fighter not found`);
+      }
+    } catch ({ message }) {
+      return (req.body = {
+        error: true,
+        message,
+      });
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.delete(
+  "/:id",
+  (req, res, next) => {
+    let id = req.params.id;
+    try {
+      const fighter = fighterService.removeFighter(id);
+      if (fighter) {
+        req.body = {
+          fighter,
+        };
+        return req.body;
+      } else {
+        throw new Error(`Fighter not found`);
+      }
+    } catch ({ message }) {
+      return (req.body = {
+        error: true,
+        message,
+      });
+    } finally {
+      next();
+    }
+  },
+
+  responseMiddleware
+);
 
 export { router };
--- services/fighterService.js
+++ services/fighterService.js
@@ -2,6 +2,65 @@ import { fighterRepository } from "../repositories/fighterRepository.js";
 
 class FighterService {
   // TODO: Implement methods to work with fighters
+  addFighter(data) {
+    const item = fighterRepository.create(data);
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+
+  updateFighter(id, dataToUpdate) {
+    const item = fighterRepository.update(id, dataToUpdate);
+
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+
+  removeFighter(id) {
+    const item = fighterRepository.delete(id);
+
+    if (item.length < 1) {
+      return null;
+    }
+    return item;
+  }
+
+  trimAndLowercaseData(str) {
+    const item = str.trim().toLowerCase();
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+  searchFighter(search) {
+    const item = fighterRepository.getOne(search);
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+  getAllFighters() {
+    const item = fighterRepository.getAll();
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+  checkIfRightField(model, object) {
+    for (const key in object) {
+      if (Object.hasOwnProperty.call(object, key)) {
+        if (key === "id") {
+          continue;
+        }
+        if (!(key in model)) {
+          throw new Error(`${key} must not be`);
+        }
+      }
+    }
+  }
 }
 
 const fighterService = new FighterService();
