47a5bbf8bef35f29e309338bd9b0037f1cfb783f
Stanislav Benet <stanislav.benet@gmail.com>
2023-06-25 22:58:14 +0300

    Add fighter and user validation

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -1,13 +1,85 @@
 import { FIGHTER } from "../models/fighter.js";
+import { fighterService } from "../services/fighterService.js";
 
 const createFighterValid = (req, res, next) => {
   // TODO: Implement validatior for FIGHTER entity during creation
-  next();
+  try {
+    fighterService.checkIfRightField(FIGHTER, req.body);
+
+    const { name, power, defense, health = 100 } = req.body;
+    console.log(name);
+    if (!name) {
+      throw new Error(`Fighter name must not be empty`);
+    } else if (!power) {
+      throw new Error(`Fighter power must not be empty`);
+    } else if (power < 1) {
+      throw new Error(`Power must be greater than 1`);
+    } else if (power > 100) {
+      throw new Error(`Power must be less than 100`);
+    } else if (!defense) {
+      throw new Error(`Fighter defense must not be empty`);
+    } else if (defense < 1) {
+      throw new Error(`Defense must be greater than 1`);
+    } else if (defense > 10) {
+      throw new Error(`Defense must be less than 10`);
+    } else {
+      const fighter = {
+        name,
+        power,
+        defense,
+        health,
+      };
+      return (req.body = fighter);
+    }
+  } catch ({ message }) {
+    req.body = {
+      error: true,
+      message,
+    };
+    return req.body;
+  } finally {
+    next();
+  }
 };
 
 const updateFighterValid = (req, res, next) => {
   // TODO: Implement validatior for FIGHTER entity during update
-  next();
+  try {
+    const { name, power, defense, health = 100 } = userAgent;
+    if (!name && !power && !defense && !health) {
+      throw new Error(`One of field must be fill`);
+    } else {
+      fighterService.checkIfRightField(FIGHTER, userAgent);
+      if (power && power < 1) {
+        throw new Error(`Power must be more than 1`);
+      } else if (power && power > 100) {
+        throw new Error(`Power must be less than 100`);
+      } else if (defense && defense < 1) {
+        throw new Error(`Defense must be more than 1`);
+      } else if (defense && defense > 10) {
+        throw new Error(`Defense must be less than 10`);
+      } else if (health && health > 12) {
+        throw new Error(`Health must be less than 120`);
+      } else if (health && health < 80) {
+        throw new Error(`Health must be more than 80`);
+      } else {
+        const fighter = {
+          name,
+          power,
+          defense,
+        };
+        return (req.body = fighter);
+      }
+    }
+  } catch ({ message }) {
+    req.body = {
+      error: true,
+      message,
+    };
+    return req.body;
+  } finally {
+    next();
+  }
 };
 
 export { createFighterValid, updateFighterValid };
--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,13 +1,94 @@
 import { USER } from "../models/user.js";
+import { userService } from "../services/userService.js";
+
+const GMAIL_REGEXP = new RegExp(/^([a-zA-Z0-9_\-\.]+)@(gmail+)\.(com)$/, "g");
+const PHONE_REGEXP = new RegExp(
+  /(\+380)?\(?\d{3}\)?[\s\.-]?(\d{7}|\d{3}[\s\.-]\d{2}[\s\.-]\d{2}|\d{3}-\d{4})/,
+  "g"
+);
 
 const createUserValid = (req, res, next) => {
   // TODO: Implement validatior for USER entity during creation
-  next();
+  try {
+    const { firstName, lastName, email, phoneNumber, password } = req.body;
+    userService.checkIfRightField(USER, req.body);
+
+    if (!firstName) {
+      throw new Error(`Error. First name must not be empty`);
+    }
+    if (!lastName) {
+      throw new Error(`Error. Last name must not bes empty`);
+    }
+    if (!email) {
+      throw new Error(`Error. Email must not be empty`);
+    }
+    if (!GMAIL_REGEXP.test(email)) {
+      throw new Error(`Error. Email is incorrect`);
+    }
+    if (!phoneNumber) {
+      throw new Error(`Error. Phone number must not be empty`);
+    }
+    if (!PHONE_REGEXP.test(phoneNumber)) {
+      throw new Error(`Error. Phone number is incorrect `);
+    }
+
+    if (!password) {
+      throw new Error(`Error. Password must not be empty`);
+    }
+    if (password.length < 3) {
+      throw new Error(`Password is too short`);
+    }
+
+    const user = {
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      password,
+    };
+    return (req.body = user);
+  } catch ({ message }) {
+    req.body = {
+      error: true,
+      message,
+    };
+    return req.body;
+  } finally {
+    next();
+  }
 };
 
 const updateUserValid = (req, res, next) => {
   // TODO: Implement validatior for user entity during update
-  next();
+  try {
+    const { firstName, lastName, email, phoneNumber, password } = req.body;
+    if (!firstName && !lastName && !email && !phoneNumber && !password) {
+      throw new Error(`One of field must be fill`);
+    } else {
+      userService.checkIfRightField(USER, req.body);
+      if (email && !GMAIL_REGEXP.test(email)) {
+        throw new Error(`Email is incorrect`);
+      }
+      if (phoneNumber && !PHONE_REGEXP.test(phoneNumber)) {
+        throw new Error(`Phone number is incorrect `);
+      }
+      if (password && password.length < 3) {
+        throw new Error(` Password is too short`);
+      }
+      const user = {
+        ...userAgent,
+      };
+      return (req.body = user);
+    }
+  } catch ({ message }) {
+    req.body = {
+      error: true,
+      message,
+    };
+    return req.body;
+  } finally {
+    next();
+  }
 };
 
 export { createUserValid, updateUserValid };
